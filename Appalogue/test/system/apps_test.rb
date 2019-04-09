71119e77e35325d6b73d55c884430451f55741da
Brad Schmitz <schmitb3@miamioh.edu>
2019-04-09 15:37:49 -0400

    Basic database connectivity created.

---
+++ Appalogue/test/system/apps_test.rb
@@ -0,0 +1,53 @@
+require "application_system_test_case"
+
+class AppsTest < ApplicationSystemTestCase
+  setup do
+    @app = apps(:one)
+  end
+
+  test "visiting the index" do
+    visit apps_url
+    assert_selector "h1", text: "Apps"
+  end
+
+  test "creating a App" do
+    visit apps_url
+    click_on "New App"
+
+    fill_in "Description", with: @app.description
+    fill_in "Name", with: @app.name
+    fill_in "Orgname", with: @app.orgName
+    fill_in "Platform", with: @app.platform
+    fill_in "Price", with: @app.price
+    fill_in "Version", with: @app.version
+    click_on "Create App"
+
+    assert_text "App was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a App" do
+    visit apps_url
+    click_on "Edit", match: :first
+
+    fill_in "Description", with: @app.description
+    fill_in "Name", with: @app.name
+    fill_in "Orgname", with: @app.orgName
+    fill_in "Platform", with: @app.platform
+    fill_in "Price", with: @app.price
+    fill_in "Version", with: @app.version
+    click_on "Update App"
+
+    assert_text "App was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a App" do
+    visit apps_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "App was successfully destroyed"
+  end
+end
