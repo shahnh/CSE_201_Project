faa0695d30d215eaf58ca8954b00029f8351b52a
Brad Schmitz <schmitb3@miamioh.edu>
2019-04-10 23:16:49 -0400

    Added the ability to upload images to each app

--- Appalogue/app/controllers/apps_controller.rb
+++ Appalogue/app/controllers/apps_controller.rb
@@ -69,6 +69,6 @@ class AppsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def app_params
-      params.require(:app).permit(:name, :description, :version, :orgName, :price, :platform)
+      params.require(:app).permit(:name, :description, :version, :orgName, :price, :platform, :image)
     end
 end
--- Appalogue/app/models/app.rb
+++ Appalogue/app/models/app.rb
@@ -1,2 +1,3 @@
 class App < ApplicationRecord
+	has_one_attached :image
 end
