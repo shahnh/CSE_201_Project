9f52ce55b81b8117b3d9e2743de5b81780c81900
shahnh <shahnh>
2019-04-14 16:29:09 -0400

    Revert "Revert "Add/Delete Comments""
    This reverts commit f19185d39788a0ac6344c00be33d7acb8abcf090.

--- Appalogue/app/controllers/comments_controller.rb
+++ Appalogue/app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+	def create
+		@app = App.find(params[:app_id])
+		@comment = @app.comments.create(comment_params)
+		redirect_to app_path(@app)
+
+	end
+
+	def destroy
+		@app = App.find(params[:app_id])
+		@comment = @app.comments.find(params[:id])
+		@comment.destroy
+		redirect_to app_path(@app)
+
+	end
+
+	private def comment_params
+		params.require(:comment).permit(:username, :body)
+
+	end 
 end
--- Appalogue/config/routes.rb
+++ Appalogue/config/routes.rb
@@ -1,5 +1,7 @@
-Rails.application.routes.draw do
-  resources :apps
+ Rails.application.routes.draw do
+  resources :apps do
+  	resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'apps#index'
 end
