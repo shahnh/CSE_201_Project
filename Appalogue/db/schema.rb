49a17e3fdd4d9aaeee1f36d43780c1ef1e579262
Ian <Brightir@miamioh.edu>
2019-04-14 15:09:07 -0400

    Added Some Comment Code

--- Appalogue/db/schema.rb
+++ Appalogue/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_192645) do
+ActiveRecord::Schema.define(version: 2019_04_13_185629) do
 
   create_table "apps", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2019_04_09_192645) do
     t.string "platform"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "username"
+    t.text "body"
+    t.integer "app_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["app_id"], name: "index_comments_on_app_id"
   end
 
 end
