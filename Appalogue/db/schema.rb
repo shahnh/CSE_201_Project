85fefeb850a5eb5c10742e9680b3eb03ca74247f
Brad Schmitz <schmitb3@miamioh.edu>
2019-04-16 23:50:02 -0400

    Added moderator flag to user class

--- Appalogue/db/schema.rb
+++ Appalogue/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_042848) do
+ActiveRecord::Schema.define(version: 2019_04_17_034652) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2019_04_16_042848) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.boolean "moderator", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
