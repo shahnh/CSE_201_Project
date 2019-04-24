f885451fdeb99308f8b400453cc6fcb3afd9fc02
shahnh <shahnh>
2019-04-23 21:38:48 -0400

    Comment Test

--- Appalogue/test/models/comment_test.rb
+++ Appalogue/test/models/comment_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ test "Comment Create" do
+ 	assert_not_nill(Comment.new)
+ end
+
+ test "Comment right"
+ user = User.new :email => "123456@miamioh.edu",
+ :password => "123456", :
+ comment = Comment.new :username => "123456@miamioh.edu"
+ :body => "It is a great app but layout need to be fix..."
+ if user.admin == true
+ 	
+
+end
+
+
 end
