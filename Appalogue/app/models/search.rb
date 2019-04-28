de4b85975be5b517b33da06ff9ae429929f3e8ad
Dan Falokun <falokudo@miamioh.edu>
2019-04-28 16:54:24 -0400

    added forms

--- Appalogue/app/models/search.rb
+++ Appalogue/app/models/search.rb
@@ -1,14 +1,15 @@
 class Search < ApplicationRecord
 
-	def search_apps
+	def search_apps 
 
 		apps = App.all
 
+		#apps = apps.where(["name LIKE ?",name]) if name.present?
 		apps = apps.where(["price == ?",price]) if price.present?
 		apps = apps.where(["description LIKE ?",description]) if description.present?
 		apps = apps.where(["platform LIKE ?",platform]) if platform.present?
-		apps = apps.where(["version == ?",platform]) if version.present?
-		apps = apps.where(["orgname LIKE ?",platform]) if orgname.present?
+		apps = apps.where(["version == ?",version]) if version.present?
+		apps = apps.where(["orgname LIKE ?",orgname]) if orgname.present?
   
 		return apps 
 	end
